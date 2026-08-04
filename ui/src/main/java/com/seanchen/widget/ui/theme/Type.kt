7bff3d3efd253ea7884abf2718a9d394acd3ab23
chenxin <3452582227@qq.com>
2026-08-04 11:36:54 +0800

    fix:修复Jitpack上传失败的bug

--- ui/src/main/java/com/seanchen/widget/ui/theme/Type.kt
+++ ui/src/main/java/com/seanchen/widget/ui/theme/Type.kt
@@ -10,7 +10,6 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import com.seanchen.widget.ui.color.*
 
 
 /**
